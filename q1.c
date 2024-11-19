7947c79e9dac3ac9e31ccd50c7173ada209b262a
trishaprajapati189 <trishaprajapati829@gmail.com>
2024-11-19 10:48:37 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x=12,y=6;
+	printf("%d + %d =%d\n",x,y,x+y);
+	printf("%d - %d =%d\n",x,y,x-y);
+	printf("%d * %d =%d\n",x,y,x*y);
+	printf("%d / %d =%d\n",x,y,x/y);
+	printf("%d % %d =%d\n",x,y,x%y);
+}
---
+++ q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x=5,y=5;
+	printf("----------\n");
+	printf("|         |\n");
+	printf("|%d * %d =%d|\n",x,y,x*y);
+	printf("|         |\n");
+	printf("----------\n");
+	
+}
